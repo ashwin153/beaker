0d4f913862c7b0f5adbeae1f8362044d71961c8a
Ashwin Madavan <ashwin.madavan@gmail.com>
2018-04-25 16:01:02 -0500

    Removed pants startup code

--- client.sh
+++ client.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 ####################################################################################################
-#                                    Install Pants Dependencies                                    #
-#              https://github.com/pantsbuild/pants/blob/master/README.md#requirements              #
-####################################################################################################
-sudo apt-get update && sudo apt-get -y install curl build-essential python python-dev openjdk-8-jdk
-
-####################################################################################################
 #                                         Bootstrap Client                                         #
 #                  https://github.com/ashwin153/beaker/tree/master/beaker-client                   #
 ####################################################################################################
